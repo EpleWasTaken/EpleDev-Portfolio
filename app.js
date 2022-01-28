4bd0950969a2e56db3384ed1eb0d69ef655fab76
Eple <71262074+EpleWasTaken@users.noreply.github.com>
2022-01-28 11:12:13 +0100

    First Test

---
+++ app.js
@@ -0,0 +1,24 @@
+const logo = document.getElementById("logo");
+
+logo.addEventListener("click", homeFunction);
+
+function homeFunction() {
+    window.location.href = "/index.html";
+}
+
+const backToTop = document.getElementById("Icon_awesome-arrow-up");
+
+window.onscroll = function() {scrollToTop()};
+
+function scrollToTop() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  } 
